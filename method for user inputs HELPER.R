be10f3cc6d5d3475918961bddcfbb6998aae48a5
Bartesto <barthuntley@iinet.net.au>
2016-02-26 13:16:42 +0800

    ok

---
+++ method for user inputs HELPER.R
@@ -0,0 +1,8 @@
+lopts <- cat("
+             1. Add an item
+             2. Delete an item
+             3. Print the list
+             4. Quit 
+             ")
+
+action <- readline("Choose an option: ")
